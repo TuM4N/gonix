9cebd4dbcbfffb659e5bf8d077d3e450b84f740c
Mr.Tricks <49667787+TuM4N@users.noreply.github.com>
2019-05-14 18:27:22 +0700

    Add files via upload

---
+++ config.php
@@ -0,0 +1,18 @@
+<?php
+/*      ==[ WARNING ]==
+     KALAU MAU NAMPILIN JUMLAH POIN KALIAN
+     ISI YANG ATAS JUGA
+ CARI URL ===[ http://gonixofs.com/api/v2/account.authorize.php ]===
+*/
+
+$data = "data=%7B%22clientId%22%3A%221%22%2C%22accountId%22%3A%221725%22%2C%22accessToken%22%3A%22341231cf7185bc3ddb6d380ee53dbf83%22%2C%22user%22%3A%22minimax%22%2C%22fcm%22%3A%22d-QeZATpM3Q%3AAPA91bGMplzsoNnfAY8kkKBiWEwLPKDSOWph6zOwZMg4EDp95Dc-122jj2dUUWHZXNy0GZFS3wPslMMO8wcEXxDtglEKjPoC_oehKZge3OJQXxcEomhhixxsQ_YUaOle5AMKyPUNzis8%22%2C%22ver%22%3A%222.0%22%2C%22pckg%22%3A%22com.goprix.app%22%7D&";
+
+/*
+   KALAU CUMA MAU DAPAT POIN ISI YANG BAWAH SAJA
+ CARI URL ===[ http://gonixofs.com/api/v2/account.Reward.php ]===
+*/
+$data1 = "data=%7B%22clientId%22%3A%221%22%2C%22accountId%22%3A%221725%22%2C%22accessToken%22%3A%2202b530551bbdd59a6048e53ead14931a%22%2C%22user%22%3A%22minimax%22%2C%22name%22%3A%225%22%2C%22value%22%3A%22Admob+Video+Credit%22%2C%22dev_name%22%3A%22iCherry+C230%22%2C%22dev_man%22%3A%22iCherry%22%2C%22ver%22%3A%222.0%22%2C%22pckg%22%3A%22com.goprix.app%22%7D&";
+
+
+
+?>
